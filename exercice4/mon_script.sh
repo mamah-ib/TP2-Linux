1e8cbc11ce137f045c93bf5fa5654af756e26691
Mamah Ibrahim <ibrahimmamah56@gmail.com>
2026-07-07 23:52:40 +0100

    Exercice 4: scripts bash automatises

---
+++ exercice4/mon_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Bienvenue dans mon script bash personnalise !"
+date
+ls -lh
---
+++ exercice4/script_interactif.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+read -p "Entrez votre nom : " nom
+echo "Bienvenue, $nom, nous sommes le $(date)"
